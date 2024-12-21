10e645d7a42ec92ad84056eacf9593c17901ee81
Nathaniel Lybrand <savorsauce@gmail.com>
2024-12-21 00:34:35 -0500

    More rearranging

--- include/version.h
+++ include/version.h
@@ -1,9 +1,9 @@
 /* Define to the library version */
-#define ALSOFT_VERSION "${LIB_VERSION}"
+#define ALSOFT_VERSION "1.24.1"
 #define ALSOFT_VERSION_NUM ${LIB_VERSION_NUM}
 
 /* Define the branch being built */
-#define ALSOFT_GIT_BRANCH "${GIT_BRANCH}"
+#define ALSOFT_GIT_BRANCH "master"
 
 /* Define the hash of the head commit */
-#define ALSOFT_GIT_COMMIT_HASH "${GIT_COMMIT_HASH}"
+#define ALSOFT_GIT_COMMIT_HASH "ff497ad11182b48d1bfa57216c51a2ac0c723c7d"
